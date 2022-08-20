1fe861f3b3fe9f3a8117ff02091065c58ed68617
OkochaEH <oussamaayachefff@gmail.com>
2022-08-20 11:23:06 +0100

    creted html and style it

---
+++ app.js
@@ -0,0 +1,11 @@
+const main = function () {
+  const song = document.querySelector(".song")
+  const play = document.querySelector('.play')
+  const outline = document.querySelector('.moving-outline .circle')
+  const
+  const
+  const
+  const
+}
+
+main()
